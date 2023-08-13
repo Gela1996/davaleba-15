bcb31a3e7e0f8faa61628be677fad940010ac2b9
gela nikabadze <gela.nikabadze.1@gmail.com>
2023-08-13 22:10:16 +0400

    1 nawili

---
+++ apps.js
@@ -0,0 +1,10 @@
+    // დავალება 1
+    
+    divElement.style.marginLeft = '30px';
+    const style = getComputedStyle(divElement);
+    const marginLeft = styles.marginLeft;
+    console.log('Computed margin-left:', marginLeft);
+
+    //დავალება 2
+
+    
